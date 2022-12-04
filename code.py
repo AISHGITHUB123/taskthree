712b1e27c2f3fca09c73c004dcd3b66311be8399
AISHGITHUB123 <105639937+AISHGITHUB123@users.noreply.github.com>
2022-12-05 02:08:02 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+def get_value(obj:dict,key_list:list):  
+    for key in key_list:
+        try:
+            obj = obj[key]
+        except KeyError:
+            return None #return none if provided object is not found
+    return obj
+
+
+obj = {'a':{'b':{'c':3}}}    # object
+key_list = list(input().split("/"))  #splitting the key at / to pass it on the above iteration
+
+print(obj,key_list) 
+print(get_value(obj,key_list))
